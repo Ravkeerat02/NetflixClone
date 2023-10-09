665c84037f5537b20104db4a3c4d99f06a123afe
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-10-09 00:37:20 -0400

    3rd party login works(google , github)

--- cloneapp/pages/auth.tsx
+++ cloneapp/pages/auth.tsx
@@ -4,11 +4,13 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import {signIn } from 'next-auth/react';
 import { useRouter } from "next/router";
+import {FcGoogle} from 'react-icons/fc';
+import {FaGithub} from 'react-icons/fa'
 
 const Auth = () => {
 
   // set and effect setup 
-  const route = useRouter();
+  const router = useRouter();
   const[email , setEmail] = useState('');
   const[name , setName] = useState('');
   const[password , setPassword] = useState('');
@@ -19,32 +21,37 @@ const Auth = () => {
     setVariant((prev) => (prev === 'login' ? 'register' : 'login'));
   },[]);
 
-  // register - used in on click
-  const register = useCallback(async() => {
-    try{
-      await axios.post('/api/register' , {
-        email , name , password
-      })
-    }catch(error){
-      console.log(error);
-    }
-  },[email , name , password])
-
-  // login funcionality 
-  const login = useCallback(async()=>{
-    try{
-      await signIn('credentials' , {
+   // login functionality
+   const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
         email,
         password,
         redirect: false,
-        callbackurl : '/'
-      })
-    }catch(error){
+        callbackUrl: '/',
+      });
+      router.push('/');
+    } catch (error) {
       console.log(error);
     }
-  },[email , password])
+  }, [email, password, router]);
 
-
+  // register - used in on click
+  const register = useCallback(
+    async () => {
+      try {
+        await axios.post('/api/register', {
+          email,
+          name,
+          password,
+        });
+        await login(); // Call the login function after successful registration
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [email, name, password, login]
+  );
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -81,7 +88,24 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button onClick={variant === 'login'  ? login : register}  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 trasition ">{variant === 'login' ? 'Login' :'Sign up'}</button>
+            <button onClick={variant === 'login'  ? login : register}  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 trasition ">
+              {variant === 'login' ? 'Login' :'Sign up'}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              {/* Google button */}
+            <div
+            onClick={() => signIn('google', { callbackUrl: '/' })} 
+            className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer hover:opacity-80 transition mr-2">
+              <FcGoogle size={30} />
+            </div>
+
+            {/* GitHub button */}
+            <div
+            onClick={() => signIn('github', { callbackUrl: '/' })} 
+            className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer hover:opacity-80 transition">
+              <FaGithub size={30} />
+            </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === 'login' ? 'First time using Netflix ?' : 'Already have an account'} 
               <span  onClick={toggleVariant} 
